d9d93a3b3f789afd3a078918efbf367078947857
mrDav90 <kangnidavidgabiam@esp.sn>
2022-05-07 00:29:48 +0000

    signin signup work

--- inscription.php
+++ inscription.php
@@ -191,6 +191,11 @@ https://templatemo.com/tm-551-stand-blog
       </div>
     </footer>
 
+
+   <!--Modal-->
+
+
+
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
